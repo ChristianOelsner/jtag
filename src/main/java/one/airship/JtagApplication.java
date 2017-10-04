7236eab68c04dbfa6d23971cbe5f1f3daa6e5d12
Christian Oelsner <christian.oelsner@outlook.com>
2017-10-04 22:26:03 +0200

    [CO]

--- src/main/java/one/airship/JtagApplication.java
+++ src/main/java/one/airship/JtagApplication.java
@@ -1,4 +1,12 @@
 package one.airship;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class JtagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JtagApplication.class, args);
+    }
 }
